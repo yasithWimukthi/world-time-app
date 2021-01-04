98cc59b7a11dfeeee44f2bd09cc960b7c5ce07b6
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-04 23:02:13 +0530

    create screens

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,1 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart/convert';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getData() async {
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : Text("Loading screen"),
+    );
+  }
+}
