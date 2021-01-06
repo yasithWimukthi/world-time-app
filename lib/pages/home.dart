4a70794f94d988130aa60103e98a794441196f63
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-06 23:40:26 +0530

    Implement interfaces

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,20 +6,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+
+    data = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       body : SafeArea(
-          child: Column(
-            children: <Widget> [
-              FlatButton.icon(
-                onPressed: (){
-                  Navigator.pushNamed(context,"/location");
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text("Edit Location"),
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget> [
+                FlatButton.icon(
+                  onPressed: (){
+                    Navigator.pushNamed(context,"/location");
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text("Edit Location"),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style : TextStyle(
+                        fontSize:28.0,
+                        letterSpacing : 2.0
+                      )
+                    )
+                  ]
+                ),
+                SizedBox(height:20),
+                Text(
+                  data['time'],
+                  style : TextStyle(
+                    fontSize:66.0
+                  )
+                )
+              ],
+            ),
           )
       )
     );
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -13,6 +14,12 @@ class _LoadingState extends State<Loading> {
   void setupWorldTime() async{
     WorldTime instance = WorldTime(location:'berlin',flag:"germeny.png",url:"Europe/Berlin");
     await instance.getTime();
+
+    Navigator.pushReplacementNamed(context, '/home',arguments:{
+      'location': instance.location,
+      'flag' : instance.flag,
+      'time' : instance.time
+    });
   }
 
   @override
@@ -24,7 +31,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : Text("Loading screen"),
+      backgroundColor : Colors.blue[900],
+      body : Center(
+        child: SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      )
     );
   }
 }
